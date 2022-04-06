82ff766baae645dc7f64ecd3c7f0d77f25f4d23b
mcokelez <maviyecokelez@gmail.com>
2022-04-06 13:23:29 +0300

    Initial Commit

---
+++ InterviewCase/InterviewCase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InterviewCase
+//
+//  Created by Maviye Çökelez on 6.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("deneme")
+    }
+
+
+}
+
